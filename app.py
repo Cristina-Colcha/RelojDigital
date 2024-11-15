9ec829864be5a96b3cc0e7d4a59c77f36f43954e
Cristina-Colcha <133615797+Cristina-Colcha@users.noreply.github.com>
2024-11-15 13:07:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,7 @@ def home():
     return render_template("index.html")  # Flask busca index.html dentro de la carpeta templates
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Obtener el puerto desde las variables de entorno, si está disponible (en Railway suele ser así)
+    port = int(os.environ.get("PORT", 5000))  # 5000 es el valor por defecto si no se encuentra el puerto
+    # Ejecutar la aplicación escuchando en todas las interfaces
+    app.run(host="0.0.0.0", port=port, debug=True)
